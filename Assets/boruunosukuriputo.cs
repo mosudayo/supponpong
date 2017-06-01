cd684de26ac8cbc17dabf2eb7e1ee3e12dad36e8
mosuuu <marubayasi-taro@udonko.achoo.jp>
2017-06-01 13:00:15 +0900

    ・当たり判定を円と線分に変更 ・自分以外の存在の色を変える

--- Assets/boruunosukuriputo.cs
+++ Assets/boruunosukuriputo.cs
@@ -30,22 +30,70 @@ public class boruunosukuriputo : NetworkBehaviour
     private Animator animator;
 
     // 距離計算用
-    float gizmox1 = 0;
-    float gizmoy1 = 0;
+    public float gizmox1 = 0;
+    public float gizmoy1 = 0;
 
     // Use this for initialization
     void Start ()
     {
         animator = GetComponent<Animator>();
     }
+    
+    public struct CircleLineCollisionResult
+    {
+        public bool Collision;
+        public Vector2 Point;
+        public Vector2 Normal;
+        public float Distance;
+    }
+
+    public static bool CircleLineCollide(Vector2 center, float radius, Vector2 lineStart, Vector2 lineEnd, ref CircleLineCollisionResult result)
+    {
+        Vector2 AC = center - lineStart;
+        Vector2 AB = lineEnd - lineStart;
+        float ab2 = AB.sqrMagnitude;
+        if (ab2 <= 0f) {
+            return false;
+        }
+        float acab = Vector2.Dot(AC, AB);
+        float t = acab / ab2;
+        if (t < 0.0f) {
+            t = 0.0f; //点Hが点Aの場所になる
+        } else if (t > 1.0f) {
+            t = 1.0f; //点Hが点Bの場所になる
+        }
+        result.Point = lineStart + t * AB;
+        result.Normal = center - result.Point;
+        float h2 = result.Normal.sqrMagnitude;
+        float r2 = radius * radius;
+        if (h2 > r2) {
+            result.Collision = false;
+        }
+        else {
+            result.Normal.Normalize();
+            result.Collision = true;
+            result.Distance = (radius - (center - result.Point).magnitude);
+        }
+        return result.Collision;
+    }
+
+
     void OnDrawGizmos()
     {
-        Gizmos.color = Color.blue;
-        Vector3 to = new Vector3(gizmox1, gizmoy1,0);
-        Vector3 from = transform.position;
+        Gizmos.color = Color.black;
+        Vector3 to = transform.position;
+        Vector3 from = new Vector3 (gizmox1,gizmoy1);
         Gizmos.DrawLine(from, to);
-        gizmox1 = from.x;
-        gizmoy1 = from.y;
+
+        Gizmos.color = Color.blue;
+         to = transform.position;
+         from = goru[0].transform.position;
+        Gizmos.DrawLine(from, to);
+
+        Gizmos.color = Color.red;
+        to = transform.position;
+        from = goru[1].transform.position;
+        Gizmos.DrawLine(from, to);
     }
         // Update is called once per frame
         void Update()
@@ -59,19 +107,45 @@ public class boruunosukuriputo : NetworkBehaviour
 
         transform.Translate(sokudox, sokudoy, 0.0F);
 
-        if (transform.position.x < -580 && sokudox <= 0) { sokudox *= -1; }
-        if (transform.position.x > 1220 && sokudox >= 0) { sokudox *= -1; }
-        if (transform.position.y < -330 && sokudoy <= 0) { sokudoy *= -1; }
-        if (transform.position.y > 740 && sokudoy >= 0) { sokudoy *= -1; }
+        bool frag = false;
+        if (transform.position.x < -580 && sokudox <= 0) { sokudox *= -1 * Random.Range(1.01f, 0.99f); frag = true; }
+        if (transform.position.x > 1220 && sokudox >= 0) { sokudox *= -1 * Random.Range(1.01f, 0.99f); frag = true; }
+        if (transform.position.y < -330 && sokudoy <= 0) { sokudoy *= -1 * Random.Range(1.01f, 0.99f); frag = true; }
+        if (transform.position.y > 740 && sokudoy >= 0) { sokudoy *= -1 * Random.Range(1.01f, 0.99f); frag = true; }
+
+        if (frag) {
+            int tinpo = Random.Range(0, 4);
+            if (tinpo == 3) {
+                float temp = sokudox;
+                sokudox = sokudoy;
+                sokudoy = temp;
+                            }
+            if (tinpo == 2) {
+                if (sokudox == 0) { sokudox = sokudoy * Random.Range(0.5f,1.0f); } else if(sokudoy == 0) { sokudoy = sokudox * Random.Range(0.5f, 1.0f); }
+            }
+        }
 
         if (sokudox != 0 || sokudoy != 0) { animator.SetFloat("speeed", 1.0f); }
         if (sokudox == 0 && sokudoy == 0) { animator.SetFloat("speeed", 0.0f); }
 
         if (isServer) {
 
-            //OnDrawGizmos();
 
-            //ゴールとボールの距離の判定(サーバーだけで計算する)
+            //ザ・ニュー接触判定
+            CircleLineCollisionResult result = new CircleLineCollisionResult();
+            CircleLineCollide(goru[0].transform.position,180, new Vector2(gizmox1, gizmoy1), transform.position, ref result);
+
+            if (result.Collision == true ) {
+                sukoa += Mathf.Abs(sokudox) + Mathf.Abs(sokudoy);
+            }
+            
+            CircleLineCollide(goru[1].transform.position, 180, new Vector2(gizmox1, gizmoy1), transform.position, ref result);
+
+            if (result.Collision == true) {
+                sukob += Mathf.Abs(sokudox) + Mathf.Abs(sokudoy);
+            }
+
+            /*ゴールとボールの距離の判定(サーバーだけで計算する)
             Vector3 Apos = goru[0].transform.position;
             Vector3 Bpos = transform.position;
             //bool fragu = false;
@@ -81,27 +155,44 @@ public class boruunosukuriputo : NetworkBehaviour
                 sukoa += Mathf.Abs(sokudox) + Mathf.Abs(sokudoy);
               //  fragu = true;
             }
-
             Apos = goru[1].transform.position;
             distance = (Apos - Bpos).sqrMagnitude;
             if (distance < 11000) {
                 sukob += Mathf.Abs(sokudox) + Mathf.Abs(sokudoy);
                 // fragu = true;                
             }
-
+            */
+            
         }
+        if (transform.position.x != gizmox1 || transform.position.y != gizmoy1 ) {
+            gizmox1 = transform.position.x;
+            gizmoy1 = transform.position.y;
+            ; }
         text.text = "あお：" + sukoa.ToString() + "てん\nあか：" + sukob.ToString() + "てん";
-
     }
     public void directhenko(float dx,float dy) {
         if (renzokuhit >= 1) { return; }
 
-        sokudox *= -1;
-        sokudoy *= -1;
+        //接触判定を満たした際，XY移動量に-1を掛けてバウンスさせる
+        sokudox *= -1* Random.Range(1.01f,0.99f);
+        sokudoy *= -1 * Random.Range(1.01f, 0.99f);
+
+        //接触判定を満たした際，たまにXY移動量を入れ替える
+        int tinpo = Random.Range(0, 4);
+        if (tinpo==3){ float temp = sokudox; sokudox = sokudoy; sokudoy = temp; }
 
         sokudox += dx*2;
         sokudoy += dy*2;
 
         renzokuhit = 10;
     }
+    public bool sessyokuhantei(Vector2 unko) {
+
+        CircleLineCollisionResult result = new CircleLineCollisionResult();
+        CircleLineCollide(unko, 100, new Vector2(gizmox1, gizmoy1), transform.position, ref result);
+
+        //text.text = result.Distance.ToString();
+        return result.Collision;
+    }
+
 }
--- Assets/hitonoido.cs
+++ Assets/hitonoido.cs
@@ -30,8 +30,56 @@ public class hitonoido : NetworkBehaviour {
         GetComponent<SpriteRenderer>().material.color = Color.green;
     }
 
-	// Update is called once per frame
-	void Update() {
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Vector3 to = transform.position;
+        Vector3 from = objA.transform.position;
+        Gizmos.DrawLine(from, to);
+        
+    }
+
+    public struct CircleLineCollisionResult
+    {
+        public bool Collision;
+        public Vector2 Point;
+        public Vector2 Normal;
+        public float Distance;
+    }
+
+    public static bool CircleLineCollide(Vector2 center, float radius, Vector2 lineStart, Vector2 lineEnd, ref CircleLineCollisionResult result)
+    {
+        Vector2 AC = center - lineStart;
+        Vector2 AB = lineEnd - lineStart;
+        float ab2 = AB.sqrMagnitude;
+        if (ab2 <= 0f) {
+            return false;
+        }
+        float acab = Vector2.Dot(AC, AB);
+        float t = acab / ab2;
+        if (t < 0.0f) {
+            t = 0.0f; //点Hが点Aの場所になる
+        }
+        else if (t > 1.0f) {
+            t = 1.0f; //点Hが点Bの場所になる
+        }
+        result.Point = lineStart + t * AB;
+        result.Normal = center - result.Point;
+        float h2 = result.Normal.sqrMagnitude;
+        float r2 = radius * radius;
+        if (h2 > r2) {
+            result.Collision = false;
+        }
+        else {
+            result.Normal.Normalize();
+            result.Distance = (radius - (center - result.Point).magnitude);
+            result.Collision = true;
+        }
+        return result.Collision;
+    }
+
+    // Update is called once per frame
+    void Update() {
 		/*
 			void Update()はホストもクライアントも両方呼ばれる。
 			なのでキー入力による移動はオブジェクトオーナーのみが行う必要がある。
@@ -61,8 +109,11 @@ public class hitonoido : NetworkBehaviour {
             if (Input.GetButton("Fire1")) {
                 GetComponent<SpriteRenderer>().material.color = Color.red;
             }
-            else { GetComponent<SpriteRenderer>().material.color = Color.green; }
+            else {
+                    GetComponent<SpriteRenderer>().material.color = Color.green;
+            }
         }
+        else { GetComponent<SpriteRenderer>().material.color = Color.white;       }
 
         /*
 			この位置で接触判定をするのであればクライアント側で行う必要がある。
@@ -73,20 +124,27 @@ public class hitonoido : NetworkBehaviour {
 			今回はdx,dyを同期するのではなく引数で渡す形とした。
 		*/
 
+        // 接触判定をボールに投げる
+
+        //早すぎる場合は線分で計算する
+        bool unti = objA.GetComponent<boruunosukuriputo>().sessyokuhantei(transform.position);
+        //unti = false;
+        
+
         //ボールとの距離の計算
         Vector3 Apos = objA.transform.position;
 		Vector3 Bpos = transform.position;
 		distance = (Apos - Bpos).sqrMagnitude;
 		//text.text = distance.ToString();
-		if ( distance < 4000 ) {
+        
+        if ( unti==true || distance < 4000 ) {
             //Fire1ボタンを押している場合，適当な数字を足して適当な数字をかけて制御不可能な速度にする
             if (Input.GetButton("Fire1")) { dx += 10; dy += 10; dx *=Random.Range(3,6) ;dy *= Random.Range(3, 6); }
             
             Cmduntitti( dx, dy ); // 関数の引数としてdx,dyを渡す
 		}
 
-
-	}
+    }
 
 	[Command]
 	void Cmduntitti( float powerX, float powerY )
